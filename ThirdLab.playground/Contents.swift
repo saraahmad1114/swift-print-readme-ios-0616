be7a4f95d30228cef1e6461c33be5a0ae6a27b46
Sara <saraahmad1114@yahoo.com>
2016-07-06 09:42:02 -0400

    print stuff and interpolation

---
+++ ThirdLab.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var favoriteWord = "mercurial"
+var favoriteColor = "blue"
+var favoriteMusician = "Nirvana"
+
+//using the print() function 
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("I'm printing a string with Swift!")
+print("Hi mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel")
+
+print("My favorite actually is \(favoriteColor).")
+
+print("Hi mom! My word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician)")
+
+var favoriteProgrammingLang = "C++"
+var favoriteSchool = "Flatiron School"
+
+print("My favorite school is\(favoriteSchool) and my favorite programming language is \(favoriteProgrammingLang)")
